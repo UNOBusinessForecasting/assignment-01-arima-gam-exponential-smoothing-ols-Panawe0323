f5869edb5b511d9e9f7b85a6eac8807eda5a9d3b
Panawe Mewenemesse <70914078+Panawe0323@users.noreply.github.com>
2024-09-14 14:01:38 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -7,23 +7,23 @@ from plotly import subplots
 import plotly.offline as py
 import plotly.graph_objs as go
 
-# Load the train and test datasets
+# Let's load the train and test datasets
 train_df = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
 test_df = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_test.csv")
 
-# Prepare the training data - Selecting year, month, day, hour as features and trips as target
+# Preparing our training data 
 X_train = train_df[['year', 'month', 'day', 'hour']]
 y_train = train_df['trips']
 
-# Prepare the test data
+# Preparing the test data
 X_test = test_df[['year', 'month', 'day', 'hour']]
 
-# Initialize and fit the GAM model (you can try different combinations of smoothing terms)
+# Initializing and fitting the GAM model
 model = LinearGAM(s(0) + s(1) + s(2) + s(3))
 
 modelFit = model.fit(X_train, y_train)
 
-# Making predictions for the test period (January 2019)
+# Making predictions for the test period
 pred = modelFit.predict(X_test)
 
 # Storing the predictions in the test DataFrame and save the result
